ec6516a201e6e8a4fa091609cc1ef12b53dc3b74
lewisbdl <89733753+lewisbdl@users.noreply.github.com>
2021-08-30 13:23:52 +0100

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -5,7 +5,7 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimiter = r"(?:-|\+){3}\s*$"  # r for Raw string
+    __delimiter = r"^(?:-|\+){3}\s*$"  # r for Raw string
     __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
