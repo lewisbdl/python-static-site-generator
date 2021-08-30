e83ddfff899cda4ee747eced3c2c075e7cf7d51e
lewisbdl <89733753+lewisbdl@users.noreply.github.com>
2021-08-30 14:08:25 +0100

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,7 +1,11 @@
 import shutil
+import sys
 
 from typing import List
 from pathlib import Path
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
 
 
 class Parser:
@@ -24,6 +28,28 @@ class Parser:
 
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
+
+
+class MarkdownParser:
+    extensions = [".md", ".markdown"]
+
+    def parse(self, path: Path, source: Path, dest: Path):  # Annotations to make the arguments Path type
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+        raise NotImplementedError
+
+
+class ReStructuredTextParser:
+    extensions = [".rst"]
+
+    def parse(self, path: Path, source: Path, dest: Path):  # Annotations to make the arguments Path type
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+        raise NotImplementedError
 
 
 class ResourceParser(Parser):
